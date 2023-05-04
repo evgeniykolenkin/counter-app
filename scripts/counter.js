b059e474e01d415f198278a8a4877251ade0da0d
Evgeny Kolenkin <immaim@yandex.ru>
2023-05-04 18:49:19 +0300

    refactoring script

--- scripts/counter.js
+++ scripts/counter.js
@@ -5,57 +5,64 @@ const counterTextExtra = document.querySelector(".counter__data-extra");
 
 let counter = 0;
 
-function ResetLoops() {
+function ResetCount() {
   counter = 0;
 
   counterText.innerText = "Передохни!";
   counterTextExtra.innerText = "Иначе никуда не поедем";
 }
 
-btnPlus.addEventListener("click", function () {
-  counter = counter + 1;
+const countLoops = () => {
+  ++counter;
 
   counterText.innerText = counter;
   counterTextExtra.innerText = "Начало положено!";
 
-  if (counter >= 5) {
+  if (counter > 0) {
+    btnReset.removeAttribute("disabled");
+  }
+  if (counter >= 5 && counter < 10) {
     counterTextExtra.innerText = "Продолжай!";
   }
-  if (counter >= 10) {
+  if (counter >= 10 && counter < 15) {
     counterTextExtra.innerText = "Неплохо!";
   }
-  if (counter >= 15) {
+  if (counter >= 15 && counter < 20) {
     counterTextExtra.innerText = "Силы еще есть?";
   }
-  if (counter >= 20) {
+  if (counter >= 20 && counter < 25) {
     counterTextExtra.innerText = "Машина!";
   }
-  if (counter >= 25) {
+  if (counter >= 25 && counter < 30) {
     counterTextExtra.innerText = "Завязывай!";
   }
-  if (counter >= 30) {
+  if (counter >= 30 && counter < 35) {
     counterTextExtra.innerText = "Я серьёзно!";
   }
-  if (counter >= 35) {
+  if (counter >= 35 && counter < 40) {
     counterTextExtra.innerText = "Ты так умрёшь скоро!";
   }
-  if (counter >= 40) {
+  if (counter >= 40 && counter < 45) {
     counterTextExtra.innerText = "Кони столько не бегают!";
   }
-  if (counter >= 45) {
+  if (counter >= 45 && counter < 50) {
     counterTextExtra.innerText = "Скорее всего ты ползёшь!";
   }
-  if (counter >= 50) {
+  if (counter >= 50 && counter < 55) {
     counterTextExtra.innerText = "Хватит!";
   }
-  if (counter >= 55) {
-    ResetLoops();
+  if (counter >= 55 && counter < 56) {
+    ResetCount();
   }
-});
+};
 
-btnReset.addEventListener("click", function () {
+const resetLoops = () => {
   counter = 0;
 
   counterText.innerText = "Погнали заново!";
   counterTextExtra.innerText = "Мы на пути к Олимпиаде!";
-});
+};
+
+btnPlus.addEventListener("click", countLoops);
+
+btnReset.addEventListener("click", resetLoops);
